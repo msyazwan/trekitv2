f7b1c81c0960db931f17a35d316e035c92622af8
syazwan <syazwan9889@gmail.com>
2020-04-29 16:57:27 +0800

    New update

--- js/auth.js
+++ js/auth.js
@@ -31,3 +31,29 @@ signupForm.addEventListener('submit', (e) => {
         signupForm.reset();
     });
 });
+
+// logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut().then(() => {
+        console.log('user signed out');
+    })
+});
+
+// login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // get user info
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+
+    // log the user in
+    auth.signInWithEmailAndPassword(email, password).then((cred) => {
+        console.log(cred.user);
+        loginForm.reset();
+    });
+
+});
